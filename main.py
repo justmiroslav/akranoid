7567361002c954f095d2090708b75fc37407d8e3
Богдан Дегтяренко <bogdandegtarenko@MacBook-Air-Bogdan.local>
2023-01-16 16:47:50 +0200

    add heart and lose

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pygame as pg
 
 WIDTH = 800
 HEIGHT = 600
+lives = 3
+game_over = False
+heart_image = pg.image.load("heart.jpg")
+heart = pg.transform.scale(heart_image, (40, 40))
+
 
 
 class Paddle:
@@ -44,6 +49,23 @@ class Ball:
         if self.y + self.radius >= HEIGHT - paddle.height:
             if paddle.x <= self.x <= paddle.x + paddle.width:
                 self.dy = -self.dy
+        if self.y >= HEIGHT - paddle.height and (self.x < paddle.x or self.x > paddle.x + paddle.width):
+            global lives
+            lives -= 1
+            if lives > 0:
+                if lives // 2:
+                    self.x = WIDTH // 2
+                    self.y = HEIGHT // 2
+                    self.dx = -8
+                    self.dy = 8
+                else:
+                    self.x = WIDTH // 2
+                    self.y = HEIGHT // 2
+                    self.dx = 8
+                    self.dy = 8
+            else:
+                global game_over
+                game_over = True
 
 
 paddle = Paddle()
@@ -52,15 +74,24 @@ ball = Ball()
 
 
 def draw():
+    global game_over
     screen.fill((255, 155, 0))
     paddle.draw()
     ball.draw()
+    if not game_over:
+        for p in range(lives):
+            screen.blit(heart, (4 + p * 50, 10))
+    if game_over:
+        screen.draw.text("GAME OVER!", center=(WIDTH // 2, HEIGHT // 2), fontsize=60, color="black")
 
 
 def update():
-    ball.update()
-
-    paddle.on_mouse_move(pg.mouse.get_pos())
+    if not game_over:
+        ball.update()
+        paddle.on_mouse_move(pg.mouse.get_pos())
+    else:
+        time.sleep(1.2)
+        pg.quit()
 
 
 pgzrun.go()
